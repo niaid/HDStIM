6540229ebfa8f28e106f9c4c6a7defcb533e4be4
Rohit Farmer <rohit.farmer@gmail.com>
2020-05-12 12:21:43 -0400

    including test for R 3.5.3

--- tests/testthat/testSCS.R
+++ tests/testthat/testSCS.R
@@ -1,7 +1,17 @@
-test_that("Whether stim_cell_selector selects the same number of cells.",{
-  selected_data <- stim_cell_selector(chi11_1k$expr_data, chi11_1k$state_markers,
-                                                chi11_1k$cluster_col, chi11_1k$stim_label,
-                                                chi11_1k$unstim_label, seed_val = 123,
-                                                umap = TRUE, umap_cells = 50)
-  expect_equal(nrow(selected_data$selected_expr_data), 16023)
-})
+if(R.Version()$minor == "5.3"){
+  test_that("Whether stim_cell_selector selects the same number of cells.",{
+    selected_data <- stim_cell_selector(chi11_1k$expr_data, chi11_1k$state_markers,
+                                        chi11_1k$cluster_col, chi11_1k$stim_label,
+                                        chi11_1k$unstim_label, seed_val = 123,
+                                        umap = TRUE, umap_cells = 50)
+    expect_equal(nrow(selected_data$selected_expr_data), 16055)
+  })
+} else if(R.Version()$minor == "6.3"){
+  test_that("Whether stim_cell_selector selects the same number of cells.",{
+    selected_data <- stim_cell_selector(chi11_1k$expr_data, chi11_1k$state_markers,
+                                        chi11_1k$cluster_col, chi11_1k$stim_label,
+                                        chi11_1k$unstim_label, seed_val = 123,
+                                        umap = TRUE, umap_cells = 50)
+    expect_equal(nrow(selected_data$selected_expr_data), 16023)
+  })
+}
