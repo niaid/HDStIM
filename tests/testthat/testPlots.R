6733ca22803bdf8ff17f49d23c238c131aa37e94
Rohit Farmer <rohit.farmer@gmail.com>
2020-07-01 22:36:47 -0400

    function for single marker selection and updated plots

--- tests/testthat/testPlots.R
+++ tests/testthat/testPlots.R
@@ -8,6 +8,15 @@ test_that("If plot_sbp returns 0.",{
   expect_equal(s, 0)
 })
 
+test_that("If plot_sbp returns 0 for single marker selection.",{
+  selected_data <- stim_cell_selector_single_marker(chi11_1k$expr_data, chi11_1k$state_markers,
+                                      chi11_1k$cluster_col, chi11_1k$stim_label,
+                                      chi11_1k$unstim_label, seed_val = 123)
+
+  s <- plot_sbp(selected_data, path = NULL, verbose = TRUE)
+  expect_equal(s, 0)
+})
+
 test_that("If plot_umap returns 0.",{
   selected_data <- stim_cell_selector(chi11_1k$expr_data, chi11_1k$state_markers,
                                       chi11_1k$cluster_col, chi11_1k$stim_label,
@@ -23,6 +32,15 @@ test_that("If plot_kde returns 0.",{
                                       chi11_1k$cluster_col, chi11_1k$stim_label,
                                       chi11_1k$unstim_label, seed_val = 123,
                                       umap = FALSE)
+
+  k <- plot_kde(chi11_1k$expr_data, selected_data, path = NULL, verbose = TRUE)
+  expect_equal(k, 0)
+})
+
+test_that("If plot_kde returns 0 for single marker.",{
+  selected_data <- stim_cell_selector_single_marker(chi11_1k$expr_data, chi11_1k$state_markers,
+                                      chi11_1k$cluster_col, chi11_1k$stim_label,
+                                      chi11_1k$unstim_label, seed_val = 123)
 
   k <- plot_kde(chi11_1k$expr_data, selected_data, path = NULL, verbose = TRUE)
   expect_equal(k, 0)
--- tests/testthat/testSCS.R
+++ tests/testthat/testSCS.R
@@ -15,3 +15,13 @@ if(R.Version()$minor == "5.3"){
     expect_equal(nrow(selected_data$selected_expr_data), 16023)
   })
 }
+
+if(R.Version()$minor == "6.3"){
+  test_that("Whether stim_cell_selector_single_marker selects the same number of cells.",{
+    selected_data <- stim_cell_selector_single_marker(chi11_1k$expr_data, chi11_1k$state_markers,
+                                                      chi11_1k$cluster_col, chi11_1k$stim_label,
+                                                      chi11_1k$unstim_label, seed_val = 123,
+                                                      verbose = TRUE)
+    expect_equal(nrow(selected_data$selected_expr_data), 58727)
+  })
+}
