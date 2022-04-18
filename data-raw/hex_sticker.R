f8a8bccbe83a0ffbddc0fb5f98c57992311915f0
Rohit Farmer <rohit.farmer@nih.gov>
2022-04-18 16:26:21 -0400

    website update

--- data-raw/hex_sticker.R
+++ data-raw/hex_sticker.R
@@ -3,7 +3,7 @@ library(hexSticker)
 library(showtext)
 
 ## Loading Google fonts (http://www.google.com/fonts)
-font <- "satisfy"
+font <- "Roboto Mono"
 font_add_google(font)
 ## Automatically use showtext to render text for future devices
 showtext_auto()
@@ -20,7 +20,7 @@ p <- ggplot(one_marker, aes(x = !!ensym(state_marker))) +
   geom_density(color="#0F3B13", fill="#486144")
 p <- p + theme_void() + theme_transparent()
 
-s <- sticker(p, package="Stim Cell Selector", p_size=5.5, p_family = font,
+s <- sticker(p, package="HDStIM", p_size=20.5, p_family = font, p_x = 1, p_y = 1.45,
              s_x=1, s_y=.8, s_width=1, s_height=0.9,
              h_fill="#E47833", h_color="#f39c12", h_size = 1, dpi = 300,
              filename=file.path("man","figures","sticker.png"))
